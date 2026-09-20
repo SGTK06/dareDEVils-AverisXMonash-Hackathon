4f2bc9694ba49c9f9335870a36da63108b3c3d38
Alpha <tejashmkumar@gmail.com>
2026-09-20 18:54:48 +0800

    chore: scaffold frontend application

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-950 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello React + TypeScript + Tailwind!
+      </h1>
+    </main>
+  )
+}
+
+export default App
